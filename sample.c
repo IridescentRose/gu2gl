d22dedb9d35e97b10d64cd64d1603a4f074731d2
Nathan Bourgeois <iridecentrosesfall@gmail.com>
2022-09-07 01:11:18 -0400

    GU2GL 1.0

--- sample.c
+++ sample.c
@@ -7,15 +7,14 @@
  */
 
 #include <pspkernel.h>
-#include <pspdisplay.h>
 #include <pspdebug.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
 
-#include <pspgu.h>
-#include <pspgum.h>
+#define GUGL_IMPLEMENTATION
+#include "gu2gl.h"
 
 static int exitRequest = 0;
 
@@ -23,13 +22,11 @@ int running()
 {
 	return !exitRequest;
 }
-
 int exitCallback(int arg1, int arg2, void *common)
 {
 	exitRequest = 1;
 	return 0;
 }
-
 int callbackThread(SceSize args, void *argp)
 {
 	int cbid;
@@ -41,7 +38,6 @@ int callbackThread(SceSize args, void *argp)
 
 	return 0;
 }
-
 int setupCallbacks(void)
 {
 	int thid = 0;
@@ -59,48 +55,6 @@ PSP_MODULE_INFO("Cube Sample", 0, 1, 1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
 
 static unsigned int __attribute__((aligned(16))) list[262144];
-
-static unsigned int staticOffset = 0;
-
-static unsigned int getMemorySize(unsigned int width, unsigned int height, unsigned int psm)
-{
-	switch (psm)
-	{
-		case GU_PSM_T4:
-			return (width * height) >> 1;
-
-		case GU_PSM_T8:
-			return width * height;
-
-		case GU_PSM_5650:
-		case GU_PSM_5551:
-		case GU_PSM_4444:
-		case GU_PSM_T16:
-			return 2 * width * height;
-
-		case GU_PSM_8888:
-		case GU_PSM_T32:
-			return 4 * width * height;
-
-		default:
-			return 0;
-	}
-}
-
-void* getStaticVramBuffer(unsigned int width, unsigned int height, unsigned int psm)
-{
-	unsigned int memSize = getMemorySize(width,height,psm);
-	void* result = (void*)staticOffset;
-	staticOffset += memSize;
-
-	return result;
-}
-
-void* getStaticVramTexture(unsigned int width, unsigned int height, unsigned int psm)
-{
-	void* result = getStaticVramBuffer(width,height,psm);
-	return (void*)(((unsigned int)result) + ((unsigned int)sceGeEdramGetAddr()));
-}
 
 struct Vertex
 {
@@ -160,89 +114,46 @@ struct Vertex __attribute__((aligned(16))) vertices[12*3] =
 	{0, 1, 0xff00007f, 1,-1,-1}, // 5
 };
 
-#define BUF_WIDTH (512)
-#define SCR_WIDTH (480)
-#define SCR_HEIGHT (272)
+
 
 int main(int argc, char* argv[])
 {
 	setupCallbacks();
 
-	// setup GU
-
-	void* fbp0 = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_8888);
-	void* fbp1 = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_8888);
-	void* zbp = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_4444);
-
-	sceGuInit();
-
-	sceGuStart(GU_DIRECT,list);
-	sceGuDrawBuffer(GU_PSM_8888,fbp0,BUF_WIDTH);
-	sceGuDispBuffer(SCR_WIDTH,SCR_HEIGHT,fbp1,BUF_WIDTH);
-	sceGuDepthBuffer(zbp,BUF_WIDTH);
-	sceGuOffset(2048 - (SCR_WIDTH/2),2048 - (SCR_HEIGHT/2));
-	sceGuViewport(2048,2048,SCR_WIDTH,SCR_HEIGHT);
-	sceGuDepthRange(65535,0);
-	sceGuScissor(0,0,SCR_WIDTH,SCR_HEIGHT);
-	sceGuEnable(GU_SCISSOR_TEST);
-	sceGuDepthFunc(GU_GEQUAL);
-	sceGuEnable(GU_DEPTH_TEST);
-	sceGuFrontFace(GU_CW);
-	sceGuShadeModel(GU_SMOOTH);
-	sceGuEnable(GU_CULL_FACE);
-	sceGuEnable(GU_CLIP_PLANES);
-	sceGuFinish();
-	sceGuSync(0,0);
-
-	sceDisplayWaitVblankStart();
-	sceGuDisplay(GU_TRUE);
-
-	// run sample
-
+    guglInit(list);
 	int val = 0;
 
 	while(running())
 	{
-		sceGuStart(GU_DIRECT,list);
+        guglStartFrame(list, GL_FALSE);
+	    glClearColor(0xff554433);
+        glClearDepth(0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glPerspective(75.0f,16.0f/9.0f,0.5f,1000.0f);
 
-		// clear screen
+		glMatrixMode(GL_VIEW);
+		glLoadIdentity();
 
-		sceGuClearColor(0xff554433);
-		sceGuClearDepth(0);
-		sceGuClear(GU_COLOR_BUFFER_BIT|GU_DEPTH_BUFFER_BIT);
+		glMatrixMode(GL_MODEL);
+		glLoadIdentity();
 
-		// setup matrices for cube
-
-		sceGumMatrixMode(GU_PROJECTION);
-		sceGumLoadIdentity();
-		sceGumPerspective(75.0f,16.0f/9.0f,0.5f,1000.0f);
-
-		sceGumMatrixMode(GU_VIEW);
-		sceGumLoadIdentity();
-
-		sceGumMatrixMode(GU_MODEL);
-		sceGumLoadIdentity();
 		{
 			ScePspFVector3 pos = { 0, 0, -2.5f };
-			ScePspFVector3 rot = { val * 0.79f * (GU_PI/180.0f), val * 0.98f * (GU_PI/180.0f), val * 1.32f * (GU_PI/180.0f) };
-			sceGumTranslate(&pos);
-			sceGumRotateXYZ(&rot);
+			ScePspFVector3 rot = { val * 0.79f * (GL_PI/180.0f), val * 0.98f * (GL_PI/180.0f), val * 1.32f * (GL_PI/180.0f) };
+			gluTranslate(&pos);
+			gluRotateXYZ(&rot);
 		}
 
-		// draw cube
+		glDrawElements(GL_TRIANGLES,GL_TEXTURE_32BITF|GL_COLOR_8888|GL_VERTEX_32BITF|GL_TRANSFORM_3D,12*3,0,vertices);
 
-		sceGumDrawArray(GU_TRIANGLES,GU_TEXTURE_32BITF|GU_COLOR_8888|GU_VERTEX_32BITF|GU_TRANSFORM_3D,12*3,0,vertices);
-
-		sceGuFinish();
-		sceGuSync(0,0);
-
-		sceDisplayWaitVblankStart();
-		sceGuSwapBuffers();
-
+        guglSwapBuffers(GL_TRUE, GL_FALSE);
 		val++;
 	}
 
-	sceGuTerm();
+    guglTerm();
 
 	sceKernelExitGame();
 	return 0;
